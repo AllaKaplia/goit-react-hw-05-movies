ce37378da9e1f2aac5e279792b4f934303fd2c48
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-05 00:30:00 +0300

    Update Home.jsx

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+
+
+const Home = () => {
+    return <div>Home page</div>
+};
+
+export default Home;
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NmVlMzk5YzgxMDA2YmVhYzdhOTFhMGFiMTVmNzc1MCIsInN1YiI6IjY0YTFhN2ViYzM5MGM1MDBjYTAwNjk1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kJeKh3R9gav2pLKibdWniRXoWeRIhWOxskbWh5h_iuI'
+//     }
+//   };
+  
+//   fetch('https://api.themoviedb.org/3/account/20096673/rated/movies?language=en-US&page=1&sort_by=created_at.asc', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
