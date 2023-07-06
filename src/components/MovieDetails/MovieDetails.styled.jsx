40c87b3c83149f9382c64d19b9c42ffa71e29417
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-06 23:47:46 +0300

    Update MovieDetails.styled.jsx

--- src/components/MovieDetails/MovieDetails.styled.jsx
+++ src/components/MovieDetails/MovieDetails.styled.jsx
@@ -1,8 +1,22 @@
-import { useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 
 const MovieDetails = () => {
-    const movieId = useParams();
-    return <div>Page with detailed information about the movie id - {movieId}</div>
+    const { movieId } = useParams();
+
+    return(
+    <main>
+        <div>Page with detailed information about the movie id - {movieId}</div>
+        <ul>
+            <li>
+                <Link to="cast">Cast</Link>
+            </li>
+            <li>
+                <Link to="reviews">Reviews</Link>
+            </li>
+        </ul>
+        <Outlet />
+    </main>
+    ) 
 }
 
 export default MovieDetails;
