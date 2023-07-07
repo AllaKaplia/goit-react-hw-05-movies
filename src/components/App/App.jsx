93d0433759fd7219f5982fc4fc1468bc75b4dc5b
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-07 22:21:15 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Cast from "components/Cast";
-import Home from "components/Home";
-import MovieDetails from "components/MovieDetails";
-import Movies from "components/Movies";
-import NotFound from "components/NotFound";
-import Reviews from "components/Reviews";
+import { lazy } from "react";
 import SharedLayout from "components/SharedLayout";
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 const App = () => {
   return (
