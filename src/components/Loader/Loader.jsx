60a0d66b0ee04bc0d6325d80369641c4e91b60c7
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-07 22:26:41 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const Loader = () => {
     return (
-        <div className="text-center">
-            <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
-        </div>
+        <MagnifyingGlass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="MagnifyingGlass-loading"
+        wrapperStyle={{}}
+        wrapperClass="MagnifyingGlass-wrapper"
+        glassColor = '#c0efff'
+        color = '#e15b64'
+        />
     )
 }
 
