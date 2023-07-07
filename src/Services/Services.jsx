f61608379cdd39e1713d552b83858dc6277c40e0
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-07 22:23:09 +0300

    Create Services.jsx

---
+++ src/Services/Services.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const KEY = '46ee399c81006beac7a91a0ab15f7750';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+export const fetchTrendingMovies = async (signal) => {
+    const response = await axios.get('trending/all/day', {
+        signal,
+        params: {
+            api_key: KEY,
+            time_window: 'week'
+        }
+    })
+    
+    return response.data;
+};
+
