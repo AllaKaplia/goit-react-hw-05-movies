9b9901a2987faa557a403336ebcc2081580652bd
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-08 01:50:48 +0300

    Create SearchBox.jsx

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,12 @@
+
+
+const SearchBar = () => {
+    return (
+        <div>
+            <input type="text" name='searchQuery'/>
+            <button type='submit'>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar;
