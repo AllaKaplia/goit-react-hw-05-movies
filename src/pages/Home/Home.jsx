e960774037e510ab2e3269ae054345bf82ebf864
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-09 02:07:46 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,48 +4,48 @@ import { useState, useEffect, useRef } from "react";
 import { fetchTrendingMovies } from "Services/Services";
 
 const Home = () => {
-    const [moviesList, setMoviesList] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [moviesList, setMoviesList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const abortController = useRef();
+  const abortController = useRef();
 
-    useEffect(() => {
-        const getTrendingWeekMovies = async () => {
-            try {
-              if (abortController.current) {
-                abortController.current.abort();
-              }
+  useEffect(() => {
+    const getTrendingWeekMovies = async () => {
+      try {
+        if (abortController.current) {
+          abortController.current.abort();
+        }
           
-              abortController.current = new AbortController();
+        abortController.current = new AbortController();
           
-              setLoading(true);
-              setError(null);
+        setLoading(true);
+        setError(null);
           
-              const movies = await fetchTrendingMovies(abortController.current.signal);
-              setMoviesList(movies);
-              setLoading(false);
-            } catch (error) {
-              if (error.name !== "AbortError") {
-                setError("Sorry, an error occurred :( Try reloading the page!");
-                setLoading(false);
-              }
-            } finally {
-                setLoading(false);
-            }
-        };
+        const movies = await fetchTrendingMovies(abortController.current.signal);
+        setMoviesList(movies);
+        setLoading(false);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          setError("Sorry, an error occurred :( Try reloading the page!");
+          setLoading(false);
+        }
+      } finally {
+          setLoading(false);
+      }
+  };
 
-        getTrendingWeekMovies();
-    }, []);
+  getTrendingWeekMovies();
+}, []);
 
-    return(
-        <div>
-            <h1>This week's movie trends</h1>
-            {loading && <Loader />}
-            {!loading && moviesList.length > 0 && <MoviesList movies={moviesList} />}
-            {error && <div>{error}</div>}
-        </div>
-    )
+  return(
+    <div>
+      <h1>This week's movie trends</h1>
+      {loading && <Loader />}
+      {!loading && moviesList.length > 0 && <MoviesList movies={moviesList} />}
+      {error && <div>{error}</div>}
+  </div>
+  )
 };
 
 export default Home;
