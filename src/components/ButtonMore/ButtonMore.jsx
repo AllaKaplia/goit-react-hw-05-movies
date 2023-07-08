16736e093123fc72909435e32d89db7fdbfc9444
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-09 02:07:28 +0300

    Create ButtonMore.jsx

---
+++ src/components/ButtonMore/ButtonMore.jsx
@@ -0,0 +1,7 @@
+import { ButtonLoad } from "./Button.styled";
+
+const ButtonMore = ({ onClick }) => {
+    return <ButtonLoad type="button" onClick={onClick} >Load more</ButtonLoad>
+}
+
+export default ButtonMore;
