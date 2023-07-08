7433dd73da6ef05455b076411a88c86190ac6827
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-09 02:07:41 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,9 +1,14 @@
 import * as yup from 'yup';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Field, Form, Formik } from 'formik';
 import { RiMovie2Line } from 'react-icons/ri';
 
 
-export default function SearchBar(){
+export default function SearchBar({ onChange }){
+    const [searchParams, setSearchParams] = useSearchParams();
+    const movieName = searchParams.get('queryMovie') ?? '';
+
     const schema = yup.object().shape({
         queryMovie: yup
         .string()
@@ -15,11 +20,26 @@ export default function SearchBar(){
         queryMovie: ''
     }
 
+    useEffect(() => {
+        if(!movieName){
+            return;
+        }
+
+        onChange(movieName);
+    }, [movieName, onChange]);
+
+    const handleSearchForm = (value) => {
+        const queryMovie = value.queryMovie;
+        const searchQuery = queryMovie !== '' ? { queryMovie } : {};
     
+        setSearchParams(searchQuery);
+        onChange(queryMovie);
+    }
    
     return (
-        <Formik initialValues={initialValue} validationSchema={schema} >
-            <Form autoComplete="off">
+        <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleSearchForm}>
+            {({ handleSubmit, errors }) => (
+            <Form autoComplete="off" onSubmit={handleSubmit}>
                 <Field 
                 type="text" 
                 name='queryMovie'
@@ -30,7 +50,9 @@ export default function SearchBar(){
                     <span>Search</span>
                     <RiMovie2Line />
                 </button>
+                {errors.queryMovie && <div>{errors.queryMovie}</div>}
             </Form>
+            )}
         </Formik>
     )
 }
