4765bf38f0166eb90cb0b0db847fc59e8dfd3cb0
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-08 20:42:17 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,12 +1,36 @@
+import * as yup from 'yup';
+import { Field, Form, Formik } from 'formik';
+import { RiMovie2Line } from 'react-icons/ri';
 
 
-const SearchBar = () => {
+export default function SearchBar(){
+    const schema = yup.object().shape({
+        queryMovie: yup
+        .string()
+        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/, 'Name is invalid')
+        .required('Name is required'),
+    })
+
+    const initialValue = {
+        queryMovie: ''
+    }
+
+    
+   
     return (
-        <div>
-            <input type="text" name='searchQuery'/>
-            <button type='submit'>Search</button>
-        </div>
+        <Formik initialValues={initialValue} validationSchema={schema} >
+            <Form autoComplete="off">
+                <Field 
+                type="text" 
+                name='queryMovie'
+                autoFocus
+                placeholder="Search movies"
+                />
+                <button type='submit'>
+                    <span>Search</span>
+                    <RiMovie2Line />
+                </button>
+            </Form>
+        </Formik>
     )
 }
-
-export default SearchBar;
