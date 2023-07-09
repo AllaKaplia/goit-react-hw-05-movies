b33296c85a28dd38d22b057fb16cd832daeece4e
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-09 18:44:36 +0300

    Update DetailsNav.jsx

--- src/components/DetailsNav/DetailsNav.jsx
+++ src/components/DetailsNav/DetailsNav.jsx
@@ -3,18 +3,38 @@ import { NavLink } from "react-router-dom";
 
 const DetailsNav = () => {
     return (
-        <div>
-            <h2>Additional information</h2>
-            <ul>
-                <li>
-                    <NavLink to='cast'>Cast</NavLink>
-                </li>
-                <li>
-                    <NavLink to='reviews'>Reviews</NavLink>
-                </li>
-            </ul>
-        </div>
+        <ul>
+            <li>
+                <NavLink to='cast'>Cast</NavLink>
+            </li>
+            <li>
+                <NavLink to='reviews'>Reviews</NavLink>
+            </li>
+        </ul>
     )
 }
 
 export default DetailsNav;
+
+// import { Link, Outlet, useParams } from "react-router-dom";
+
+// const MovieDetails = () => {
+//     const { movieId } = useParams();
+
+//     return(
+//     <main>
+//         <div>Page with detailed information about the movie id - {movieId}</div>
+//         <ul>
+//             <li>
+//                 <Link to="cast">Cast</Link>
+//             </li>
+//             <li>
+//                 <Link to="reviews">Reviews</Link>
+//             </li>
+//         </ul>
+//         <Outlet />
+//     </main>
+//     ) 
+// }
+
+// export default MovieDetails;
