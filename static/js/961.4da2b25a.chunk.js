17ff3e7b4b3f3ecc310195fd96782dd5a1bf0327
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-07-09 14:09:22 +0000

    Deploying to gh-pages from @ AllaKaplia/goit-react-hw-05-movies@df6ea3a79b1de4c73f33625921b571a2d6f2dcc3 🚀

--- static/js/961.4da2b25a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[961],{7961:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"Movie Details"})}}}]);
-//# sourceMappingURL=961.4da2b25a.chunk.js.map
