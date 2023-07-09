6624fc6f207f077a17bd747e304df036fc74d4c4
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-09 21:50:51 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -3,11 +3,10 @@ import { Link, useLocation } from "react-router-dom";
 const MoviesList = ({ moviesTrending, moviesSearch }) => {
     const location = useLocation();
 
-    const movies = moviesTrending || moviesSearch || [];
-
     return (
         <ul>
-            {movies.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
+            {moviesTrending ? 
+            (moviesTrending.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
                 <li key={id}>
                     <Link to={`movies/${id}`} state={{ from: location }}>
                         <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
@@ -16,7 +15,16 @@ const MoviesList = ({ moviesTrending, moviesSearch }) => {
                         <p>Release date: {release_date}</p>
                     </Link>
                 </li>
-            ))}
+            ))) : (moviesSearch.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
+                <li key={id}>
+                    <Link to={`movies/${id}`} state={{ from: location }}>
+                        <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
+                        <h2>{original_title}</h2>
+                        <p>Original language: {original_language}</p>
+                        <p>Release date: {release_date}</p>
+                    </Link>
+                </li>
+            )))}
         </ul>
     );
 };
