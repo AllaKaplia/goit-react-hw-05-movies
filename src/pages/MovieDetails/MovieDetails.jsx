0b3ab50e88045f404dea13f6ab76c6ee419408c7
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-09 18:44:51 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,84 +1,89 @@
 import { Outlet, useLocation, useParams } from "react-router-dom";
-import { useState,  useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import GoBackLink from "components/GoBackLink";
-import { fetchMovieDetails } from "Services/Services";
 import DetailsNav from "components/DetailsNav";
 import { Suspense } from "react";
 import Loader from "components/Loader";
 import MovieInfo from "pages/MovieInfo";
-
+import { fetchMovieDetails } from "Services/Services";
 
 const MovieDetails = () => {
-    const [moviesDetails, setMoviesDetails] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const abortController = useRef();
-    const { movieId } = useParams();
+  const [moviesDetails, setMoviesDetails] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const abortController = useRef();
+  const movieId = useParams();
 
-    const location = useLocation();
-    const goBackLink = useRef(location?.state?.from ?? '/');
+  const location = useLocation();
+  const goBackLink = useRef(location?.state?.from ?? "/");
 
-    useEffect(() => {
-        const getMovieDetails = async () => {
-            try {
-                if (abortController.current) {
-                    abortController.current.abort();
-                }
-            
-                abortController.current = new AbortController();
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      try {
+        if (abortController.current) {
+          abortController.current.abort();
+        }
 
-                setLoading(true);
-                setError(null);
+        abortController.current = new AbortController();
 
-                const movieInfo = await fetchMovieDetails(movieId, abortController.current.signal);
+        setLoading(true);
+        setError(null);
 
-                setMoviesDetails(movieInfo);
-                console.log(movieInfo);
-                setError(null);
+        const movieInfo = await fetchMovieDetails(
+          movieId,
+          abortController.current.signal
+        );
 
-            } catch (error) {
-                if (error.name !== "AbortError") {
-                    setError("Sorry, an error occurred :( Try reloading the page!");
-                    setLoading(false);
-                }
-            } finally {
-                setLoading(false);
-            }
-        };
+        setMoviesDetails(movieInfo);
+        setError(null);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          setError(
+            "Sorry, an error occurred :( Try reloading the page!"
+          );
+          setLoading(false);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        getMovieDetails();
-    }, [movieId]);
+    getMovieDetails();
+  }, [movieId]);
 
-    const { 
-        title,
-        release_date,
-        overview,
-        genres,
-        poster_path,
-        original_title,
-        vote_average
-    } = moviesDetails || {};
-    
-    return (
-        <div>
-            <GoBackLink to={goBackLink.current}/>
-            {!loading && moviesDetails && title && release_date && overview && genres && poster_path && original_title && vote_average && (<MovieInfo
-                title={title}
-                release={release_date}
-                overview={overview}
-                genres={genres}
-                src={poster_path}
-                originalTitle={original_title}
-                rating={vote_average}
-            />)}
+  const {
+    title,
+    release_date,
+    overview,
+    genres,
+    poster_path,
+    original_title,
+    vote_average,
+  } = moviesDetails || {};
 
-            <DetailsNav />
-            {error && <div>{error}</div> }
-            <Suspense fallback={<Loader />}>
-                <Outlet />
-            </Suspense>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Additional information</h2>     
+      <GoBackLink to={goBackLink.current} />
+      {!loading && moviesDetails && title && release_date && overview && genres && poster_path && original_title && vote_average && (
+        <MovieInfo
+          title={title}
+          release={release_date}
+          overview={overview}
+          genres={genres}
+          src={poster_path}
+          originalTitle={original_title}
+          rating={vote_average}
+        />
+      )}
+
+      <DetailsNav />
+      {error && <div>{error}</div>}
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
 
 export default MovieDetails;
