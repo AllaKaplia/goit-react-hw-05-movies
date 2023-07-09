06b968832933c2608f231b5cd546e9ffbc7efe4c
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-10 01:18:58 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,32 +1,23 @@
 import { Link, useLocation } from "react-router-dom";
 
-const MoviesList = ({ moviesTrending, moviesSearch }) => {
+const MoviesList = ({ movies }) => {
     const location = useLocation();
-
+  
     return (
-        <ul>
-            {moviesTrending ? 
-            (moviesTrending.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
-                <li key={id}>
-                    <Link to={`movies/${id}`} state={{ from: location }}>
-                        <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
-                        <h2>{original_title}</h2>
-                        <p>Original language: {original_language}</p>
-                        <p>Release date: {release_date}</p>
-                    </Link>
-                </li>
-            ))) : (moviesSearch.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
-                <li key={id}>
-                    <Link to={`movies/${id}`} state={{ from: location }}>
-                        <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
-                        <h2>{original_title}</h2>
-                        <p>Original language: {original_language}</p>
-                        <p>Release date: {release_date}</p>
-                    </Link>
-                </li>
-            )))}
-        </ul>
+      <ul>
+        {movies.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
+          <li key={id}>
+            <Link to={`movies/${id}`} state={{ from: location }}>
+              <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
+              <h2>{original_title}</h2>
+              <p>Original language: {original_language}</p>
+              <p>Release date: {release_date}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     );
-};
+  };
+  
 
 export default MoviesList;
