ddb5ed23b2596c4837f5f1b6f1a71fffe7796438
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-11 00:02:01 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,56 +3,53 @@ import { useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Field, Form, Formik } from 'formik';
 import { RiMovie2Line } from 'react-icons/ri';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+export default function SearchBar({ onChange }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieName = searchParams.get('queryMovie') ?? '';
 
-export default function SearchBar({ onChange }){
-    const [searchParams, setSearchParams] = useSearchParams();
-    const movieName = searchParams.get('queryMovie') ?? '';
+  const schema = yup.object().shape({
+    queryMovie: yup
+      .string()
+      .matches(/^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я]+)*$/, 'Name is invalid')
+      .required('Name is required'),
+  });
 
-    const schema = yup.object().shape({
-        queryMovie: yup
-        .string()
-        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/, 'Name is invalid')
-        .required('Name is required'),
-    })
+  const initialValue = {
+    queryMovie: movieName,
+  };
 
-    const initialValue = {
-        queryMovie: ''
+  useEffect(() => {
+    if (!movieName) {
+      return;
     }
 
-    useEffect(() => {
-        if(!movieName){
-            return;
-        }
+    onChange(movieName);
+  }, [movieName, onChange]);
 
-        onChange(movieName);
-    }, [movieName, onChange]);
+  const handleSearchForm = (values, { resetForm }) => {
+    const queryMovie = values.queryMovie;
+    const searchQuery = queryMovie !== '' ? { queryMovie } : {};
 
-    const handleSearchForm = (value) => {
-        const queryMovie = value.queryMovie;
-        const searchQuery = queryMovie !== '' ? { queryMovie } : {};
-    
-        setSearchParams(searchQuery);
-        onChange(queryMovie);
-    }
-   
-    return (
-        <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleSearchForm}>
-            {({ handleSubmit, errors }) => (
-            <Form autoComplete="off" onSubmit={handleSubmit}>
-                <Field 
-                type="text" 
-                name='queryMovie'
-                autoFocus
-                placeholder="Search movies"
-                />
-                <button type='submit'>
-                    <span>Search</span>
-                    <RiMovie2Line size={24} />
-                </button>
-                {errors.queryMovie && <div>{errors.message}</div>}
-            </Form>
-            )}
-        </Formik>
-    )
+    setSearchParams(searchQuery);
+    onChange(queryMovie);
+    resetForm();
+  };
+  
+  return (
+    <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleSearchForm}>
+      {({ handleSubmit, errors }) => (
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Field type="text" name="queryMovie" autoFocus placeholder="Search movies" />
+          <button type="submit">
+            <span>Search</span>
+            <RiMovie2Line size={24} />
+          </button>
+          {errors.queryMovie && toast.error(errors.queryMovie.message)}
+        </Form>
+      )}
+    </Formik>
+  );
 }
