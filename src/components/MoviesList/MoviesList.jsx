a7f547c7353478876a494fd32989eb514f755225
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-10 15:44:10 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -7,7 +7,7 @@ const MoviesList = ({ movies }) => {
       <ul>
         {movies.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
           <li key={id}>
-            <Link to={`movies/${id}`} state={{ from: location }}>
+            <Link to={`/movies/${id}`} state={{ from: location }}>
               <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
               <h2>{original_title}</h2>
               <p>Original language: {original_language}</p>
