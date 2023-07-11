e25812e15e4986f3b48787c7b2b08d3f44de88f5
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-11 15:43:19 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,21 +1,29 @@
 import { Link, useLocation } from "react-router-dom";
+import { ContainerDescription, Data, GalleryMovies, Language, MovieItem, OriginalTitle } from "./MoviesList.styled";
+import placeholderImage from "../../imgPlaceholder/imgPlaceholder.png";
 
 const MoviesList = ({ movies }) => {
     const location = useLocation();
   
     return (
-      <ul>
+      <GalleryMovies>
         {movies.map(({ id, poster_path, original_language, title, release_date, original_title }) => (
-          <li key={id}>
+          <MovieItem key={id}>
             <Link to={`/movies/${id}`} state={{ from: location }}>
-              <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} width="200" />
-              <h2>{original_title}</h2>
-              <p>Original language: {original_language}</p>
-              <p>Release date: {release_date}</p>
+              <img 
+              src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : placeholderImage} 
+              alt={title} 
+              width="220"
+              />
+            <ContainerDescription>
+              <OriginalTitle>{original_title}</OriginalTitle>
+              <Language>Original language: {original_language}</Language>
+              <Data>Release date: {release_date}</Data>
+            </ContainerDescription>
             </Link>
-          </li>
+          </MovieItem>
         ))}
-      </ul>
+      </GalleryMovies>
     );
   };
   
