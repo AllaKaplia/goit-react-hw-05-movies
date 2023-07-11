b24971df7e531bb90c2e17972fe3a3171e7247cb
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-11 15:44:07 +0300

    Update MovieInfo.jsx

--- src/pages/MovieInfo/MovieInfo.jsx
+++ src/pages/MovieInfo/MovieInfo.jsx
@@ -1,9 +1,9 @@
-
+import placeholderImage from "../../imgPlaceholder/imgPlaceholder.png";
 
 const MovieInfo = ({ title, release, overview, genres, src, originalTitle, rating }) => {
     return (
         <div>
-            <img src={`https://image.tmdb.org/t/p/w500${src}`} alt={originalTitle} width={300} />
+            <img src={src ? `https://image.tmdb.org/t/p/w500${src}` : placeholderImage} alt={originalTitle} width={300} />
             <div>
                 <h1>{title} {release}</h1>
                 <p>User rating: {Math.floor(rating * 10)}%</p>
