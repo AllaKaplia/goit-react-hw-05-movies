d5f554adcdd7f2e261a88225906b4bb49f2b6f61
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-11 15:43:09 +0300

    Update DetailsNav.jsx

--- src/components/DetailsNav/DetailsNav.jsx
+++ src/components/DetailsNav/DetailsNav.jsx
@@ -3,17 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const DetailsNav = () => {
     return (
-        <div>
-            <h2>Additional information</h2>
-            <ul>
-                <li>
-                    <NavLink to='cast'>Cast</NavLink>
-                </li>
-                <li>
-                    <NavLink to='reviews'>Reviews</NavLink>
-                </li>
-            </ul>
-        </div>
+        <ul>
+            <li>
+                <NavLink to='cast'>Cast</NavLink>
+            </li>
+            <li>
+                <NavLink to='reviews'>Reviews</NavLink>
+            </li>
+        </ul>
     )
 }
 
