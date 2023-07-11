12b80b280c82c9e30e60e40ef2f4be9b44cde132
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-11 21:15:22 +0300

    Update Reviews.styled.jsx

---
+++ src/components/Reviews/Reviews.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Message = styled.div`
+    font-size: 25px;
+    color: green;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+    margin-bottom: 30px;
+`;
