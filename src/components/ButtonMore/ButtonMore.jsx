fbc0ef886261a1bbe31b0b72dc048a62c604affd
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-11 15:42:56 +0300

    Delete ButtonMore.jsx

--- src/components/ButtonMore/ButtonMore.jsx
+++
@@ -1,7 +0,0 @@
-import { ButtonLoad } from "./Button.styled";
-
-const ButtonMore = ({ onClick }) => {
-    return <ButtonLoad type="button" onClick={onClick} >Load more</ButtonLoad>
-}
-
-export default ButtonMore;
