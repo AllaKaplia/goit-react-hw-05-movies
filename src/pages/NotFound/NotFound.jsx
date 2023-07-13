a9e485fbe2710ebe057adf7e0ccd6496b6bf4fd2
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-13 22:15:02 +0300

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,22 @@
-import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import { GiExitDoor } from 'react-icons/gi';
+import { BackBox, BackBtn, TextNotFound } from "./NotFound.styled";
+
 
 
 const NotFound = () => {
-    return toast.success('Unfortunately, no such page was found :(');
+
+    return (
+        <BackBox>
+            <Link to="/">
+                <BackBtn type="button">
+                    <GiExitDoor />
+                    Go Back Home
+                </BackBtn>
+            </Link>
+            <TextNotFound>Unfortunately, no such page was found :(</TextNotFound> 
+        </BackBox>
+    );
 }
 
 export default NotFound;
