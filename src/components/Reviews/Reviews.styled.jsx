f9e81fd5f9231a815f115bbf385305ad4a46bb84
Alla Kaplia <kaplia.alla18@gmail.com>
2023-08-01 16:46:01 +0300

    Update Reviews.styled.jsx

--- src/components/Reviews/Reviews.styled.jsx
+++ src/components/Reviews/Reviews.styled.jsx
@@ -4,13 +4,13 @@ export const ReviewsLi = styled.li`
     box-shadow: rgba(46, 47, 66, 0.08) 0px 10px 10px, rgba(46, 47, 66, 0.16) 0px 1px 1px, rgba(46, 47, 66, 0.08) 0px 2px 1px;
     padding: 10px;
     border-radius: 5px;
-    background-color: #bff1c2;
+    background-color: #fcb689;
     margin-bottom: 20px;
 `;
 
 export const Message = styled.div`
     font-size: 25px;
-    color: green;
+    color: #b31bb4;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
     margin-bottom: 30px;
 `;
@@ -21,7 +21,7 @@ export const Author = styled.h3`
     font-size: 25px;
     line-height: 1.2;
     letter-spacing: 0.02em;
-    color: #057205;
+    color: #b31bb4;
 `;
 
 export const Descriptions = styled.p`
@@ -30,5 +30,5 @@ export const Descriptions = styled.p`
     font-size: 18px;
     line-height: 1.2;
     letter-spacing: 0.02em;
-    color: #057205;
+    color: #b31bb4;
 `;
